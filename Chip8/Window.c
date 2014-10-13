90696af8d4f0d4b280d26246aad0c3aa0ffc2769
Spl3en <spl3en.contact@gmail.com>
2014-10-13 21:24:00 +0200

    Window events handling in Window (main thread).

---
+++ Chip8/Window.c
@@ -0,0 +1,291 @@
+#include "Window.h"
+#include <stdlib.h>
+
+// ---------- Debugging -------------
+#define __DEBUG_OBJECT__ "Window"
+#include "dbg/dbg.h"
+
+// Singleton
+Window *g_this = NULL;
+
+/*
+ * Description 	: Allocate a new Window structure.
+ * Return		: A pointer to an allocated Window.
+ */
+Window *
+Window_new (void)
+{
+	Window *this;
+
+	if (g_this != NULL) {
+		dbg ("Error : A window has already been instanciated.");
+		return NULL;
+	}
+
+	if ((this = calloc (1, sizeof(Window))) == NULL)
+		return NULL;
+
+	if (!Window_init (this)) {
+		Window_free (this);
+		return NULL;
+	}
+
+	g_this = this;
+
+	return this;
+}
+
+
+/*
+ * Description : Initialize an allocated Window structure.
+ * Window *this : An allocated Window to initialize.
+ * Return : true on success, false on failure.
+ */
+bool
+Window_init (
+	Window *this
+) {
+	// Configure SFML RenderWindow
+	this->sfmlWindow = sfRenderWindow_create (
+		(sfVideoMode) {
+			.width  = RESOLUTION_W * PIXEL_SIZE,
+			.height = RESOLUTION_H * PIXEL_SIZE,
+			.bitsPerPixel = 32
+		},
+		WINDOW_TITLE,
+		(WINDOW_FULLSCREEN) ? sfFullscreen : sfNone,
+		(sfContextSettings[]) {{
+			.depthBits = 32,
+			.stencilBits = 8,
+			.antialiasingLevel = 0,
+			.majorVersion = 2,
+			.minorVersion = 1,
+		}}
+	);
+
+	// Activate vertical sync
+	sfRenderWindow_setVerticalSyncEnabled (this->sfmlWindow, true);
+	sfRenderWindow_setActive (this->sfmlWindow, false);
+
+	// Reset keys state
+	memset (this->keysState, KEY_RELEASED, sizeof (this->keysState));
+
+	// Ready state
+	this->isRunning = true;
+
+	return true;
+}
+
+
+/*
+ * Description : Request the Window to emit a beep
+ * Window *this : An allocated Window
+ * Return : void
+ */
+void
+Window_requestBeep (void) {
+	g_this->beepRequest = true;
+}
+
+
+/*
+ * Description : Set a key at a given state
+ * C8KeyCode code : The key targeted
+ * KeyState state : The new state of the key
+ * Return : void
+ */
+void
+Window_setKeyState (
+	C8KeyCode code,
+	KeyState state
+) {
+	g_this->keysState[code] = state;
+}
+
+
+/*
+ * Description : Retrieve the state of a given key
+ * C8KeyCode code : The requested key
+ * Return : keyState
+ */
+KeyState
+Window_requestKeyState (
+	C8KeyCode code
+) {
+	return g_this->keysState[code];
+}
+
+
+/*
+ * Description : Start the main loop of the Window in a separate thread.
+ * Window *this : An allocated Window
+ * Return : sfThread * Thread object pointer
+ */
+sfThread *
+Window_startThread (
+	Window *this
+) {
+	this->thread = sfThread_create ((void (*)(void*)) Window_loop, this);
+	sfThread_launch (this->thread);
+
+	return this->thread;
+}
+
+
+/*
+ * Description : Stop the separate thread for the Window
+ * Window *this : An allocated Window
+ * Return : void
+ */
+void
+Window_stopThread (
+	Window *this
+) {
+	this->isRunning = false;
+	sfThread_wait (this->thread);
+}
+
+
+/*
+ * Description :
+ * Window *this : An allocated Window
+ * Return : void
+ */
+void
+Window_loop (
+	Window *this
+) {
+	sfEvent event;
+
+	// Association CHIP-8 keycode <-> SFML KeyCode
+	C8KeyCode sfmlToC8Codes [] = {
+		[sfKeyNum1] = keyCode_1,
+		[sfKeyNum2] = keyCode_2,
+		[sfKeyNum3] = keyCode_3,
+		[sfKeyNum4] = keyCode_4,
+		[sfKeyA] = keyCode_A,
+		[sfKeyZ] = keyCode_Z,
+		[sfKeyE] = keyCode_E,
+		[sfKeyR] = keyCode_R,
+		[sfKeyQ] = keyCode_Q,
+		[sfKeyS] = keyCode_S,
+		[sfKeyD] = keyCode_D,
+		[sfKeyC] = keyCode_C,
+		[sfKeyW] = keyCode_W,
+		[sfKeyX] = keyCode_X,
+		[sfKeyF] = keyCode_F,
+		[sfKeyV] = keyCode_V
+	};
+
+	// Poll SFML window events
+	while (this->isRunning)
+	{
+        while (sfRenderWindow_pollEvent (this->sfmlWindow, &event))
+        {
+			switch (event.type)
+			{
+				case sfEvtClosed:
+					this->isRunning = false;
+					break;
+				break;
+
+				case sfEvtKeyPressed:
+				case sfEvtKeyReleased:
+					switch (event.key.code) {
+						case sfKeyEscape:
+							// ESCAPE : Quit
+							this->isRunning = false;
+							break;
+						break;
+
+						case sfKeyNum1:
+						case sfKeyNum2:
+						case sfKeyNum3:
+						case sfKeyNum4:
+						case sfKeyA:
+						case sfKeyZ:
+						case sfKeyE:
+						case sfKeyR:
+						case sfKeyQ:
+						case sfKeyS:
+						case sfKeyD:
+						case sfKeyW:
+						case sfKeyX:
+						case sfKeyC:
+						case sfKeyV: {
+							C8KeyCode code = sfmlToC8Codes[event.key.code];
+							if (event.type == sfEvtKeyPressed) {
+								switch (this->keysState[code]) {
+									case KEY_PRESSED:
+										// Don't accept inputs already pushed, set the key state in a waiting state
+										this->keysState[code] = KEY_PUSHED;
+									break;
+
+									case KEY_RELEASED:
+										this->keysState[code] = KEY_PRESSED;
+									break;
+
+									default: // KEY_PUSHED : Do nothing
+									break;
+								}
+							} else {
+								this->keysState[code] = KEY_RELEASED;
+							}
+						}
+						break;
+
+						default:
+							dbg ("Warning : keycode = '%x' unhandled", event.key.code);
+						break;
+					}
+				break;
+
+				default :
+				break;
+			}
+        }
+
+		// Check if a beep is requested
+		if (this->beepRequest) {
+			#ifdef WIN32
+				Beep (440, 120);
+			#else
+				dbg ("Beep !");
+			#endif
+
+			this->beepRequest = false;
+		}
+
+		// Sleep a bit so the CPU doesn't burn
+		sfSleep(sfMilliseconds(1));
+	}
+
+}
+
+/*
+ * Description : Free an allocated Window structure.
+ * Window *this : An allocated Window to free.
+ */
+void
+Window_free (
+	Window *this
+) {
+	if (this != NULL)
+	{
+		free (this);
+	}
+}
+
+
+/*
+ * Description : Unit tests checking if a Window is coherent
+ * Window *this : The instance to test
+ * Return : true on success, false on failure
+ */
+bool
+Window_test (
+	Window *this
+) {
+
+	return true;
+}
---
+++ Chip8/Window.h
@@ -0,0 +1,173 @@
+// --- Author : Moreau Cyril - Spl3en
+#pragma once
+
+// ---------- Includes ------------
+#include "Utils/Utils.h"
+#include "Screen.h"
+#include <SFML/Graphics.h>
+#include <stdint.h>
+
+// ---------- Defines -------------
+#define KEYS_COUNT 16
+
+// Window properties
+#define WINDOW_TITLE 		"CHIP-8 Emulator"
+#define WINDOW_FULLSCREEN 	false
+
+
+// ------ Structure declaration -------
+
+/*
+ *	Key associated with each keycode
+ */
+typedef enum {
+	keyCode_X = 0x0,
+	keyCode_1 = 0x1,
+	keyCode_2 = 0x2,
+	keyCode_3 = 0x3,
+	keyCode_A = 0x4,
+	keyCode_Z = 0x5,
+	keyCode_E = 0x6,
+	keyCode_Q = 0x7,
+	keyCode_S = 0x8,
+	keyCode_D = 0x9,
+	keyCode_W = 0xA,
+	keyCode_C = 0xB,
+	keyCode_4 = 0xC,
+	keyCode_R = 0xD,
+	keyCode_F = 0xE,
+	keyCode_V = 0xF,
+
+	keyCodeCount // Always at the end
+} C8KeyCode;
+
+typedef enum {
+
+	KEY_RELEASED,
+	KEY_PRESSED,
+	KEY_PUSHED
+
+} KeyState;
+
+typedef struct _Window
+{
+	// SFML window object
+	sfRenderWindow *sfmlWindow;
+
+	// Keys states
+	uint8_t keysState [KEYS_COUNT];
+
+	// Running state
+	bool isRunning;
+
+	// Flag true if CPU requested a beep
+	bool beepRequest;
+
+	// Thread object pointer
+	sfThread *thread;
+
+}	Window;
+
+
+
+// --------- Allocators ---------
+
+/*
+ * Description 	: Allocate a new Window structure.
+ * Return		: A pointer to an allocated Window.
+ */
+Window *
+Window_new (void);
+
+// ----------- Functions ------------
+
+/*
+ * Description : Initialize an allocated Window structure.
+ * Window *this : An allocated Window to initialize.
+ */
+bool
+Window_init (
+	Window *this
+);
+
+/*
+ * Description : Unit tests checking if a Window is coherent
+ * Window *this : The instance to test
+ * Return : true on success, false on failure
+ */
+bool
+Window_test (
+	Window *this
+);
+
+/*
+ * Description :
+ * Window *this : An allocated Window
+ * Return : void
+ */
+void
+Window_loop (
+	Window *this
+);
+
+/*
+ * Description : Stop the separate thread for the Window
+ * Window *this : An allocated Window
+ * Return : void
+ */
+void
+Window_stopThread (
+	Window *this
+);
+
+/*
+ * Description : Start the main loop of the Window in a separate thread.
+ * Window *this : An allocated Window
+ * Return : sfThread * Thread object pointer
+ */
+sfThread *
+Window_startThread (
+	Window *this
+);
+
+/*
+ * Description : Request the Window to emit a beep
+ * Return : void
+ */
+void
+Window_requestBeep (void);
+
+/*
+ * Description : Retrieve the state of a given key
+ * C8KeyCode code : The requested key
+ * Return : keyState
+ */
+KeyState
+Window_requestKeyState (
+	C8KeyCode code
+);
+
+/*
+ * Description : Set a key at a given state
+ * C8KeyCode code : The key targeted
+ * KeyState state : The new state of the key
+ * Return : void
+ */
+void
+Window_setKeyState (
+	C8KeyCode code,
+	KeyState state
+);
+
+// --------- Destructors ----------
+
+/*
+ * Description : Free an allocated Window structure.
+ * Window *this : An allocated Window to free.
+ */
+void
+Window_free (
+	Window *this
+);
+
+
