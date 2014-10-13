5dfceaee098d24c20d8142599dc09ee86ef140bf
Spl3en <spl3en.contact@gmail.com>
2014-10-13 21:53:17 +0200

    Remove default command line name.

--- main.c
+++ main.c
@@ -5,9 +5,6 @@ int main (int argc, char **argv)
 	Window *window;
 	Cpu *cpu;
 
-	argc = 2;
-	argv[1] = "./games/INVADERS";
-
 	if (argc < 2) {
 		printf ("Usage : %s <game>\n", get_filename (argv[0]));
 		return 0;
